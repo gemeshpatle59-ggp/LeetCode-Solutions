af433640ac3aeb49b7d97f8cc803a09c70b84238
Gemeshp <gemeshpatle59@gmail.com>
2026-08-29 10:38:13 +0530

    Add LeetCode Question 1 - Two Sum

---
+++ Array/01_two_sum.py
@@ -0,0 +1,52 @@
+"""
+You are given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+ 
+
+Constraints:
+
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+
+"""
+
+nums = [2,7,11,15]
+target = 9
+
+class Solution:
+    def twoSum(self, nums, target):
+        n = len(nums)
+        hash_map = {}
+
+        for i in range(n):
+            remaining = target - nums[i]
+            if remaining in hash_map:
+                return [hash_map[remaining],i]
+            hash_map[nums[i]] = i    
+
+
+a1 = Solution()
+print(a1.twoSum(nums,target))
+            
+        
