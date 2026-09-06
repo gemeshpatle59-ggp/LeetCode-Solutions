99e19756a1afe9fcc7460e91ce5a6d372dd84085
Gemeshp <gemeshpatle59@gmail.com>
2026-09-06 21:27:47 +0530

    Add LeetCode Question 115 - Distinct Subsequences

---
+++ DALY_LeetCode.py/115.py
@@ -0,0 +1,56 @@
+"""
+Given two strings s and t, return the number of distinct subsequences of s which equals t.
+
+The test cases are generated so that the answer fits on a 32-bit signed integer.
+
+ 
+
+Example 1:
+
+Input: s = "rabbbit", t = "rabbit"
+Output: 3
+Explanation:
+As shown below, there are 3 ways you can generate "rabbit" from s.
+rabbbit
+rabbbit
+rabbbit
+Example 2:
+
+Input: s = "babgbag", t = "bag"
+Output: 5
+Explanation:
+As shown below, there are 5 ways you can generate "bag" from s.
+babgbag
+babgbag
+babgbag
+babgbag
+babgbag
+ 
+
+Constraints:
+
+1 <= s.length, t.length <= 1000
+s and t consist of English letters.
+ 
+
+
+"""
+
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        n = len(t)
+
+        dp = [0] * (n + 1)
+        dp[0] = 1
+
+        for ch in s:
+            for j in range(n, 0, -1):
+                if ch == t[j - 1]:
+                    dp[j] += dp[j - 1]
+
+        return dp[n]
+
+
+
+a1 = Solution()
+print(a1.numDistinct("babgbag","bag"))  
